166b90e7cc7e80e3b0698cfe7d5f8df5ecfad1e7
malistov.artem <malistov.artem@russianpost.ru>
2022-09-25 14:45:44 +0300

    remarks in  Task005

--- Seminar001/Task005.py
+++ Seminar001/Task005.py
@@ -1,6 +1,18 @@
-a = int(input('Введите число: '))
+# метод для запроса у пользователя целых чисел с проверкой на целочисленность
+def input_int():
+    try:
+        num = int(input(f"Введите число: "))
+        return int(num)
+    except ValueError:
+        quit(print('Ошибка. Вы ввели не целое число.'))
 
-if ((a%5 == 0 and a%10 ==0) or a%15 == 0) and a%30 != 0:
-    print('Условие выполняется')
-else:
-    print('Условие не выполняется')
+
+# метод для проверки выполнения условия
+def check_condition(num):
+    if ((num % 5 == 0 and num % 10 == 0) or num % 15 == 0) and num % 30 != 0:
+        print('Условие выполняется')
+    else:
+        print('Условие не выполняется')
+
+
+check_condition(input_int())
