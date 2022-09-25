9eba95f7727e859474ab88d42102e679f713d464
malistov.artem <malistov.artem@russianpost.ru>
2022-09-25 14:37:02 +0300

    remarks in  Task002

--- Seminar001/Task002.py
+++ Seminar001/Task002.py
@@ -1,15 +1,33 @@
-count = 0
-list=[]
-while count < 5:
-    list.insert(count, int(input('Введите число: ')))
-    count +=1
+# метод для запроса у пользователя целых чисел с проверкой на целочисленность
+def input_int(order):
+    try:
+        a = int(input(f"Введите {order}-е число: "))
+        return int(a)
+    except ValueError:
+        quit(print('Ошибка. Вы ввели не целое число.'))
 
-i = 1
-max = list[0]
 
-while i < len(list):
-    if list[i] > max:
-        max = list[i]
-    i += 1
+# метод для заполнения массива целыми числами через запрос у пользователя ввести их с клавиатуры
+def list_insert(dig_amount):
+    counter = 0
+    list = []
+    while counter < dig_amount:
+        list.insert(counter, input_int(counter + 1))
+        counter +=1
+    return list
 
-print('Максимальное число = ', max)
+
+# метод для поиска максимального числа во входном массиве
+def find_max(list):
+    i = 1
+    max = list[0]
+
+    while i < len(list):
+        if list[i] > max:
+            max = list[i]
+        i += 1
+    return max
+
+
+digits_amount = 5
+print('Максимальное число = ', find_max(list_insert(digits_amount)))
