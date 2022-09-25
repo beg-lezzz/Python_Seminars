8fcf58221f80009de6eb55aff24490465201f40c
malistov.artem <malistov.artem@russianpost.ru>
2022-09-25 21:25:18 +0300

    add HomeWork Task001

--- HomeWork001/Task001.py
+++ HomeWork001/Task001.py
@@ -10,11 +10,7 @@ def input_int():
 
 # метод проверяет какой день недели соответствует введенному числу и проверяет выходной это день или будний
 def check_day_number(day_number):
-    day_list = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
-    if day_number in (6, 7):
-        print(f"{day_list[day_number - 1]} - Выходной")
-    else:
-        print(f"{day_list[day_number - 1]} - Будний")
+    print("Да") if day_number in (6, 7) else print("Нет")
 
 
 check_day_number(input_int())
