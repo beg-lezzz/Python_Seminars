abd36cef28bdd98b08086c4faf3e5a8f834c3110
malistov.artem <malistov.artem@russianpost.ru>
2022-09-25 14:37:21 +0300

    remarks in  Task003

--- Seminar001/Task003.py
+++ Seminar001/Task003.py
@@ -1,11 +1,27 @@
-a = int(input('Введите число: '))
+# метод для запроса у пользователя целых чисел с проверкой на целочисленность
+def input_int():
+    try:
+        num = int(input(f"Введите число: "))
+        return int(num)
+    except ValueError:
+        quit(print('Ошибка. Вы ввели не целое число.'))
 
-list=[]
-i = -a
-len = 1
-while i < a:
-    list.insert(len, i)
-    len+=1
-    i+=1
 
-print(list)
+# метод для заполнения списка числами от -N до N
+def fill_list(input_number):
+    list = []
+    i = -input_number
+    len = 1
+    # в зависимости от знака введенного числа запускаем цикл либо на увеличение, либо на уменьшение
+    while i <= input_number if input_number > 0 else i >= input_number:
+        list.insert(len, i)
+        len += 1
+        # в зависимости от знака введенного числа либо инкрементируем, либо декрементируем i
+        if input_number > 0:
+            i += 1
+        else:
+            i -= 1
+    return list
+
+
+print(fill_list(input_int()))
