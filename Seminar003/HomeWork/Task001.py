44cf3a4cea3815354b906b153049e75d5fbbd144
malistov.artem <malistov.artem@russianpost.ru>
2022-10-04 19:32:00 +0300

    add HomeWork3 Task001

---
+++ Seminar003/HomeWork/Task001.py
@@ -0,0 +1,26 @@
+def input_list():
+    return input("Введите элементы списка через пробелы: ").split(' ')
+
+
+def check_list(input_list):
+    for i in input_list:
+        if not i.isnumeric():
+            return False
+        else:
+            return True
+
+
+def odd_index_sum(input_list):
+    if check_list(input_list):
+        if len(input_list) > 2:
+            product_nums = 1
+            for i in range(1, len(input_list), 2):
+                product_nums *= int(input_list[i])
+            return 'Произведение элементов на нечетных позициях = ' + str(product_nums)
+        else:
+            return 'Нет элементов на нечетных позициях'
+    else:
+        return 'В списке содержатся не только числа, либо список пуст'
+
+
+print(odd_index_sum(input_list()))
