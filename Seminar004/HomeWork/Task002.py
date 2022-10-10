1977ac969f4c55489ed29ff8dbd68dd9149e6db3
malistov.artem <malistov.artem@russianpost.ru>
2022-10-10 14:58:56 +0300

    add HomeWork4 Task003

---
+++ Seminar004/HomeWork/Task002.py
@@ -0,0 +1,31 @@
+import math
+
+
+# метод для запроса у пользователя чисел с проверкой на условия
+def input_int():
+    try:
+        num = int(input(f"Введите число для разложения на простые множители: "))
+        return num if num > 1 else \
+            quit('Ошибка. Введите целое число больше 1.')
+    except ValueError:
+        quit('Ошибка. Введите целое число больше 1.')
+
+
+# метод для разложения введенного числа на простые множители
+def prime_factors(input_num):
+    prime_factors_list = []
+    # проходим по всем числам от 2 до введенного
+    for i in range(2, input_num + 1):
+        # пока полученное число делится на текущее значение i без остатка, записываем этот делитель в список
+        while not input_num % i:
+            prime_factors_list.append(i)
+            input_num //= i
+    return prime_factors_list
+
+
+# метод для вывода результатов пользователю
+def main(input_list):
+    print('\n' + '{} = {}' .format(math.prod(input_list), ' * '.join(map(str, input_list))))
+
+
+main(prime_factors(input_int()))
---
+++ Seminar004/HomeWork/Task003.py
@@ -0,0 +1,40 @@
+import random
+
+
+# метод для запроса у пользователя чисел с проверкой на условия
+def input_int():
+    try:
+        num = int(input(f"Введите число элементов списка: "))
+        return num if num > 1 else \
+            quit('Ошибка. Введите целое число больше 1.')
+    except ValueError:
+        quit('Ошибка. Введите целое число больше 1.')
+
+
+# метод для заполнения списка случайными числами в количестве, введенном пользователем
+def fill_list_rnd(input_num):
+    input_list = []
+    for i in range(input_num):
+        input_list.append(random.randint(0, 10))
+    return input_list
+
+
+# метод для создания списка из элементов исходного списка, которые не повторяются
+def exclude_duplicates(input_list):
+    type(input_list)
+    new_list = []
+    for i in input_list:
+        if input_list.count(i) < 2:
+            new_list.append(i)
+    return input_list, new_list
+
+
+# метод для вывода результата пользователю
+def main(input_list):
+    print('\n' + '*' * 15 + ' Исходный список ' + '*' * 15 + '\n' + ', '.join(str(x) for x in input_list[0]) + '\n' * 2
+          + '*' * 15 + ' Список неповторяющихся ' + '*' * 15 + '\n'  + ', '.join(str(x) for x in sorted(input_list[1])) + '\n' * 2
+          + '*' * 15 + ' Список уникальных ' + '*' * 15 + '\n'  + ', '.join(str(x) for x in sorted(set(input_list[0]))) + '\n')
+
+
+main(exclude_duplicates(fill_list_rnd(input_int())))
+
