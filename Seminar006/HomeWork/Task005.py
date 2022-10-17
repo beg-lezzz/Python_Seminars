de48d06bbc0697b082f1f0da733faa964f1dbd1d
malistov.artem <malistov.artem@russianpost.ru>
2022-10-18 01:01:58 +0300

    add remarks in Seminar6 Task005

--- Seminar006/HomeWork/Task005.py
+++ Seminar006/HomeWork/Task005.py
@@ -4,6 +4,6 @@
 input_list = [int(element) for element in input("Введите элементы списка через пробелы: ").split(' ')
               if element.isnumeric()]
 
-not_double = list(filter(lambda x: input_list.count(x) < 2, input_list))
+not_double = list(filter(lambda a: input_list.count(a) < 2, input_list))
 
 print(not_double)
